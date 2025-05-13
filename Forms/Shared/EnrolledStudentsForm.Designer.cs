2988f547ae09df34f06872236a5f2641b6c021be
KKien2005 <suprominhkien@gmail.com>
2025-05-13 22:28:41 +0700

    Color and course

--- Forms/Shared/EnrolledStudentsForm.Designer.cs
+++ Forms/Shared/EnrolledStudentsForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace CNPM.Forms
+﻿namespace CNPM.Forms.Shared
 {
     partial class EnrolledStudentsForm
     {
@@ -28,35 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // dataGridViewStudents
-            // 
-            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewStudents.Location = new System.Drawing.Point(-1, 1);
-            this.dataGridViewStudents.Name = "dataGridViewStudents";
-            this.dataGridViewStudents.RowHeadersWidth = 51;
-            this.dataGridViewStudents.RowTemplate.Height = 24;
-            this.dataGridViewStudents.Size = new System.Drawing.Size(802, 449);
-            this.dataGridViewStudents.TabIndex = 0;
-            // 
-            // EnrolledStudentsForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridViewStudents);
-            this.Name = "EnrolledStudentsForm";
-            this.Text = "EnrolledStudentsForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Text = "Manage Enrolled Students";
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridViewStudents;
     }
 }
